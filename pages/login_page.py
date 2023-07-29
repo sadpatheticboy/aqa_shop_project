6004b2f530d94d2ccdb79453f9d31f98f437ba51
sadpatheticboy <bal-tim@mail.ru>
2023-07-29 16:47:38 +0300

    add authorization

---
+++ pages/login_page.py
@@ -0,0 +1,52 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from base.base_class import Base
+
+
+class LoginPage(Base):
+    # Base URL
+    url = 'https://www.saucedemo.com/'
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators
+    username = "//input[@id='user-name']"
+    password = "//input[@id='password']"
+    login_button = "//input[@id='login-button']"
+
+    # Getters
+    def get_username(self):
+        return WebDriverWait(self.driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, self.username)))
+
+    def get_password(self):
+        return WebDriverWait(self.driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, self.password)))
+
+    def get_login_button(self):
+        return WebDriverWait(self.driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, self.login_button)))
+
+    # Actions
+    def input_username(self, username_input):
+        self.get_username().send_keys(username_input)
+        print('Input Username')
+
+    def input_password(self, password_input):
+        self.get_password().send_keys(password_input)
+        print('Input Password')
+
+    def click_login_button(self):
+        self.get_login_button().click()
+        print('Click Login Button')
+
+    # Methods
+    def authorization(self):
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        self.input_username('standard_user')
+        self.input_password('secret_sauce')
+        self.click_login_button()
---
+++ tests/test_buy_product.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from pages.login_page import LoginPage
+
+
+def test_buy_product():
+    # Подключение к Chrome
+    driver = webdriver.ChromeOptions()
+    driver.add_experimental_option('detach', True)
+    path_chrome = Service('D:/Necessary/QA/drivers')
+    driver = webdriver.Chrome(options=driver, service=path_chrome)
+    print('Start Test')
+
+    login = LoginPage(driver)
+    login.authorization()
+
+    enter_cart = WebDriverWait(driver, 10).until(
+        expected_conditions.element_to_be_clickable((By.XPATH, "//div[@id='shopping_cart_container']")))
+    enter_cart.click()  # Вход в корзину
+    print('Click cart')
+
