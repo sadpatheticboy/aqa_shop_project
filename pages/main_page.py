4372f8c823b5fd3420b0334cc8ab6d12bed2740a
sadpatheticboy <bal-tim@mail.ru>
2023-07-31 19:50:59 +0300

    add test order for products 1, 2, 3

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,15 +10,25 @@ class MainPage(Base):
         self.driver = driver
 
     # Locators
-    add_to_cart_button = "//button[@id='add-to-cart-sauce-labs-backpack']"
+    add_to_cart_button_1 = "//button[@id='add-to-cart-sauce-labs-backpack']"
+    add_to_cart_button_2 = "//button[@id='add-to-cart-sauce-labs-bike-light']"
+    add_to_cart_button_3 = "//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']"
     cart_button = "//div[@id='shopping_cart_container']"
     menu_button = "//button[@id='react-burger-menu-btn']"
     link_about = "//a[@id='about_sidebar_link']"
 
     # Getters
-    def get_add_to_cart_button(self):
+    def get_add_to_cart_button_1(self):
         return WebDriverWait(self.driver, 10).until(
-            expected_conditions.element_to_be_clickable((By.XPATH, self.add_to_cart_button)))
+            expected_conditions.element_to_be_clickable((By.XPATH, self.add_to_cart_button_1)))
+
+    def get_add_to_cart_button_2(self):
+        return WebDriverWait(self.driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, self.add_to_cart_button_2)))
+
+    def get_add_to_cart_button_3(self):
+        return WebDriverWait(self.driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, self.add_to_cart_button_3)))
 
     def get_cart_button(self):
         return WebDriverWait(self.driver, 10).until(
@@ -33,9 +43,17 @@ class MainPage(Base):
             expected_conditions.element_to_be_clickable((By.XPATH, self.link_about)))
 
     # Actions
-    def click_add_to_cart_button(self):
-        self.get_add_to_cart_button().click()
-        print('Click Add To Cart Button')
+    def click_add_to_cart_button_1(self):
+        self.get_add_to_cart_button_1().click()
+        print('Click Add To Cart Button 1')
+
+    def click_add_to_cart_button_2(self):
+        self.get_add_to_cart_button_2().click()
+        print('Click Add To Cart Button 2')
+
+    def click_add_to_cart_button_3(self):
+        self.get_add_to_cart_button_3().click()
+        print('Click Add To Cart Button 3')
 
     def click_cart_button(self):
         self.get_cart_button().click()
@@ -50,9 +68,19 @@ class MainPage(Base):
         print('Click Link About')
 
     # Methods
-    def select_product(self):
+    def select_product_1(self):
         self.get_current_url()
-        self.click_add_to_cart_button()
+        self.click_add_to_cart_button_1()
+        self.click_cart_button()
+
+    def select_product_2(self):
+        self.get_current_url()
+        self.click_add_to_cart_button_2()
+        self.click_cart_button()
+
+    def select_product_3(self):
+        self.get_current_url()
+        self.click_add_to_cart_button_3()
         self.click_cart_button()
 
     def go_to_menu_about(self):
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -1,3 +1,6 @@
+import time
+import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -9,28 +12,96 @@ from pages.overview_page import OverviewPage
 from pages.finish_page import FinishPage
 
 
-def test_buy_product():
+@pytest.mark.run(order=3)
+def test_buy_product_1():
     # Подключение к Chrome
     driver = webdriver.ChromeOptions()
     driver.add_experimental_option('detach', True)
     path_chrome = Service('D:/Necessary/QA/drivers')
     driver = webdriver.Chrome(options=driver, service=path_chrome)
-    print('Start Test')
+    print('Start Test 1')
 
     lp = LoginPage(driver)
     lp.authorization()
 
-    mp = MainPage(driver)  # mp = Main Page
-    mp.select_product()
+    mp = MainPage(driver)
+    mp.select_product_1()
 
     cp = CartPage(driver)
     cp.product_confirm()
 
-    uip = UserInformationPage(driver)
-    uip.input_user_information()
+    print('Finish Test 1')
+    time.sleep(1)
+    driver.quit()
 
-    ov = OverviewPage(driver)
-    ov.payment()
 
-    fp = FinishPage(driver)
-    fp.finish()
+@pytest.mark.run(order=1)
+def test_buy_product_2():
+    # Подключение к Chrome
+    driver = webdriver.ChromeOptions()
+    driver.add_experimental_option('detach', True)
+    path_chrome = Service('D:/Necessary/QA/drivers')
+    driver = webdriver.Chrome(options=driver, service=path_chrome)
+    print('Start Test 2')
+
+    lp = LoginPage(driver)
+    lp.authorization()
+
+    mp = MainPage(driver)
+    mp.select_product_2()
+
+    cp = CartPage(driver)
+    cp.product_confirm()
+
+    print('Finish Test 2')
+    time.sleep(1)
+    driver.quit()
+
+
+@pytest.mark.run(order=2)
+def test_buy_product_3():
+    # Подключение к Chrome
+    driver = webdriver.ChromeOptions()
+    driver.add_experimental_option('detach', True)
+    path_chrome = Service('D:/Necessary/QA/drivers')
+    driver = webdriver.Chrome(options=driver, service=path_chrome)
+    print('Start Test 3')
+
+    lp = LoginPage(driver)
+    lp.authorization()
+
+    mp = MainPage(driver)
+    mp.select_product_3()
+
+    cp = CartPage(driver)
+    cp.product_confirm()
+
+    print('Finish Test 3')
+    time.sleep(1)
+    driver.quit()
+
+# def test_buy_product():
+#     # Подключение к Chrome
+#     driver = webdriver.ChromeOptions()
+#     driver.add_experimental_option('detach', True)
+#     path_chrome = Service('D:/Necessary/QA/drivers')
+#     driver = webdriver.Chrome(options=driver, service=path_chrome)
+#     print('Start Test')
+#
+#     lp = LoginPage(driver)
+#     lp.authorization()
+#
+#     mp = MainPage(driver)
+#     mp.select_product()
+#
+#     cp = CartPage(driver)
+#     cp.product_confirm()
+#
+#     uip = UserInformationPage(driver)
+#     uip.input_user_information()
+#
+#     ov = OverviewPage(driver)
+#     ov.payment()
+#
+#     fp = FinishPage(driver)
+#     fp.finish()
